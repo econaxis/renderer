ef5ee46d9f4e4d101bce41f4aafbea1b55b108b2
ksitu0 <59377604+ksitu0@users.noreply.github.com>
2021-04-19 18:19:37 -0700

    fxaa no functional gurantee

---
+++ pixeller.cpp
@@ -0,0 +1,102 @@
+#include <ostream>
+#include "image.h"
+#include <gmtl/Matrix.h>
+#include <gmtl/MatrixOps.h>
+
+
+#include "sfml_header.h"
+
+#include "obj_loader.h"
+
+#include "utils.h"
+#include "light.h"
+#include <thread>
+#include "renderscene.h"
+#include "camera.h"
+#include <cstdint>
+#include "fxaa.cpp"
+
+using namespace std::chrono_literals;
+
+RenderScene *main_scene = nullptr;
+std::unique_ptr<Image> image;
+Camera camera;
+Model* model = new Model("../head.obj");
+Light *light;
+extern "C" int do_render();
+
+int main(int argc, char *argv[]) {
+    std::cout << "Script starting\n" << std::endl;
+
+    std::size_t width = 1000, height = 800;
+    std::string filename = "teddy.obj";
+
+    if (argc == 4) {
+        filename = argv[1];
+        width = std::stoi(argv[2]);
+        height = std::stoi(argv[3]);
+    }
+    image = std::make_unique<Image>(width, height);
+    image->clear();
+
+    gmtl::Matrix44f persp = create_perspective_transform_matrix();
+    gmtl::Matrix44f screen = create_screen_matrix(image->width, image->height);
+
+    gmtl::Matrix44f screen_persp = screen * persp;
+
+
+    light = new Light(image->width, image->height);
+//    light->bake_light(*model);
+
+
+    // Lighting constants. Changing it changes the specific properties of the object (e.g. rubber/plastic/metal/wood...)
+    int specular_selectivity = 5;
+    double k_reflectivity = 0.2;
+
+    main_scene = new RenderScene{*model, *light, camera, *image, k_reflectivity, specular_selectivity, screen_persp};
+    setup_callbacks();
+
+    int r = 2000;
+    while(true) {
+        std::this_thread::sleep_for(30ms);
+        main_scene->main_render_code();
+    }
+
+    std::cout<<"done\n";
+    return 0;
+}
+#ifndef HAS_SFML
+extern "C" {
+EMSCRIPTEN_KEEPALIVE
+std::uintptr_t image_data_loc() {
+    return (std::uintptr_t) image->display.image_data_loc;
+}
+
+
+EMSCRIPTEN_KEEPALIVE
+int do_render() {
+    main_scene->image.clear();
+    gmtl::Matrix44f bogus{};
+    poll_input(main_scene->image, bogus);
+//    main_scene->screen_persp = screen * persp;
+
+    main_scene->main_render_code();
+    return 1;
+}
+
+EMSCRIPTEN_KEEPALIVE
+void render_light_view() {
+    light->bake_light(main_scene->model);
+    main_scene->image.display.render(main_scene->light.get_image());
+}
+
+
+EMSCRIPTEN_KEEPALIVE
+void input() {
+    main_scene->handle_input();
+}
+
+
+
+}
+#endif
